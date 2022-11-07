8832dde0239dc27b86b259c77022f52baba4b047
thejashwinitheju <thejashwinitheju912@gmail.com>
2022-11-07 17:21:51 +0530

    account

---
+++ calculator/class/account.js
@@ -0,0 +1,13 @@
+class Account{
+    min_bal=500;
+open_account(){
+    console.log("accountopend")
+}
+user_name(){
+    console.log("theju")
+}
+}
+let a1=new Account();
+console.log(a1.min_bal);
+a1.open_account();
+a1.user_name();
