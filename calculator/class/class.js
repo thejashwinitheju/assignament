595c7b8e00fcf264345e89da0b3d9fe0cac9a154
thejashwinitheju <thejashwinitheju912@gmail.com>
2022-11-07 17:20:17 +0530

    class

---
+++ calculator/class/class.js
@@ -0,0 +1,21 @@
+class Employee{
+    details_emp=100;
+    user_name(){
+console.log("thejashwini")
+    }
+    phone_nO(){
+console.log("9148698492")
+    }
+    salary_amt(){
+console.log("50000")
+    }
+    adress_home(){
+console.log("banglore")
+    }
+}
+let a1=new Employee();
+console.log(a1.details_emp);
+a1.user_name();
+a1.phone_nO();
+a1.salary_amt();
+a1.adress_home();
